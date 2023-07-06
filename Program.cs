de39f6d48b1d9bee092906bec0db7c334ce656d8
lucasmenchon <developer@luccas.dev>
2023-07-06 20:34:21 -0300

    change version and deploy, add yml

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
+using BlazorApp.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using DotNetDocsShowDemo;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
